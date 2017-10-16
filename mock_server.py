fcd4a29d0a2e552bd59645632114f7e7963a839e
ajbone <ajbone@sina.com>
2017-10-16 13:16:26 +0800

    init new project (first commit)

---
+++ mock_server.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+from flask import jsonify, Flask,make_response,request
+import pymysql,sys
+import ConfigParser
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+app = Flask(__name__)
+
+cf = ConfigParser.ConfigParser()
+path = 'db.config'
+cf.read(path)
+cf.read(path)
+secs = cf.sections()
+_host= cf.get("database","dbhost")
+_port= cf.get("database","dbport")
+_dbname=cf.get("database","dbname")
+_dbuser=cf.get("database","dbuser")
+_dbpassword=cf.get("database","dbpassword")
+_path=cf.get("path","filepath")
+
+config ={
+        'host':_host,
+        'port':int(_port),
+        'user':_dbuser,
+        'passwd':_dbpassword,
+        'db':_dbname,
+        'charset':'utf8',
+        }
+
+def checksize(domain,method):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    size = cur.execute('select * from mock_config where domain=%s', (domain))  # 校验domain是否存在
+    size1 = cur.execute('select * from mock_config where methods=%s', (method))  # 校验method是否存在
+    conn.close()
+    if size == 0:
+        return jsonify({"msg": "请求方法不存在"})
+    elif size1 == 0:
+        return jsonify({"msg": "请求方法对应的请求模式不存在"})
+
+def checkpath(domain,varsvalue,method):
+    method=method.lower()
+    varsvalue.sort()
+    #print ("domain is %s,method is %s" % (domain,method))
+    checksize(domain,method)#判断请求方法和模式是否匹配
+    if len(varsvalue) == 0:
+        conn = pymysql.connect(**config)
+        cur = conn.cursor()
+        cur.execute('select resparams from mock_config where status=0 and domain=%s and methods=%s', (domain, method))
+        resparams = cur.fetchone()
+        conn.close()
+        if resparams[0] == '':
+            return jsonify({"msg": "对应请求没有配置预期返回值"})
+        else:
+            return resparams[0].encode("utf-8")
+    else:
+        varsvalue1=getvar(varsvalue)#实际请求
+        conn = pymysql.connect(**config)
+        cur = conn.cursor()
+        cur.execute('select reqparams,resparams,methods,ischeck from mock_config where status=0 and domain=%s and methods=%s',(domain, method))
+        reqparams = cur.fetchall()
+        if reqparams == ():
+            return jsonify({"msg": u"请求方法和参数不匹配"})
+        elif reqparams[0][3]==1:
+            return reqparams[0][1]
+        else:
+            rdata=checkparams(reqparams,varsvalue1)
+        return rdata
+
+def checkparams(reqparams,varsvalue1):
+    varsvalue2 = reqparams[0][0]  # 数据库中的预期请求参数
+    if reqparams[0][2].lower()=='get' or (reqparams[0][2].lower()=='post' and varsvalue1[0] != '}' and varsvalue1[-2] != '}'):
+        arr = varsvalue2.split('&')
+        for i in range(len(arr)):
+            arr[i] = arr[i] + '&'
+        arr.sort(reverse=True)
+        str = ''.join(arr)[0:-1]
+        if str==varsvalue1:
+            return reqparams[0][1].encode("utf-8")
+        if reqparams[0][0] == '':
+            return jsonify({"msg": u"对应请求没有配置预期返回值"})
+        else:
+            return jsonify({"msg": u"请求方法和参数不匹配"})
+    elif reqparams[0][2].lower()=='post':
+        varsvalue1 = varsvalue1.replace("\t", "").replace("\r", "").strip()[:-1]
+        varsvalue2 = varsvalue2.replace("\t", "").replace("\r", "").strip()
+        if varsvalue1 == varsvalue2:
+            return reqparams[0][1].encode("utf-8")
+    else:
+        return jsonify({"msg": u"暂不支持该类型请求方法"})
+
+def getvar(value):
+    value=value[::-1]
+    result = ''
+    f = 0
+    for i in range(len(value)):
+        for j in range(len(value[i])):
+            if f % 2 == 0:
+                result = result + value[i][j] + '='
+                f = f + 1
+            else:
+                result = result + value[i][j] + '&'
+                f = f + 1
+    return result[0:-1]
+
+
+# @app.route('/<path:path>/<path:path1>', methods=['GET','POST'])
+# def get_all_task(path,path1):
+#     npath='/' + path + '/' + path1
+#     print npath
+#     if request.method=='GET':
+#         varsvalue = request.args.items()
+#         print "11111111"
+#         print varsvalue
+#     else:
+#         varsvalue = request.form.items()
+#     r = checkpath(npath, varsvalue, request.method)
+#     return r
+
+@app.route('/<path:path>', methods=['GET','POST'])
+def get_all_task1(path):
+    path = '/' + path
+    if request.method=='GET':
+        varsvalue = request.args.items()
+    else:
+        varsvalue = request.form.items()
+    r = checkpath(path, varsvalue, request.method)
+    return r
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'msg':'fail','error': '404 Not found'}), 404)
+
+@app.errorhandler(500)
+def not_found(error):
+    return make_response("程序报错,无法访问", 500)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True, port=8089,threaded=True)
---
+++ mock_web.py
@@ -0,0 +1,251 @@
+#coding=utf-8
+from  flask import Flask,request,jsonify,make_response,abort
+from flask_cors import *
+import pymysql,xlrd
+from flask_restful import reqparse
+from datetime import datetime
+import ConfigParser
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+cf = ConfigParser.ConfigParser()
+path = 'db.config'
+cf.read(path)
+cf.read(path)
+secs = cf.sections()
+_host= cf.get("database","dbhost")
+_port= cf.get("database","dbport")
+_dbname=cf.get("database","dbname")
+_dbuser=cf.get("database","dbuser")
+_dbpassword=cf.get("database","dbpassword")
+_path=cf.get("path","filepath")
+
+config ={
+        'host':_host,
+        'port':int(_port),
+        'user':_dbuser,
+        'passwd':_dbpassword,
+        'db':_dbname,
+        'charset':'utf8',
+        }
+
+save_path=_path
+ALLOWED_EXTENSIONS = ['xls', 'xlsx']
+app=Flask(__name__)
+CORS(app, supports_credentials=True)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/import_excel', methods=[ 'POST'])
+def import_device():
+    file = request.files['files[]']
+    filename = file.filename
+    # 判断文件名是否合规
+    if file and allowed_file(filename):
+        file.save(save_path+filename)
+        excelName = save_path+filename
+        bk = xlrd.open_workbook(excelName, encoding_override="utf-8")
+        sh = bk.sheets()[0]  # 因为Excel里只有sheet1有数据，如果都有可以使用注释掉的语句
+        ncols = sh.ncols#列
+        nrows = sh.nrows#行
+        conn = pymysql.connect(**config)
+        cur = conn.cursor()
+        for j in range(1,nrows):
+            if j+1 == nrows:
+                return jsonify({'msg': "ok", "remark": "上传成功"})
+            else:
+                lvalues = sh.row_values(j+1)
+                if lvalues[6]=='是':
+                    ischeck = 0
+                elif lvalues[6]=='否':
+                    ischeck = 1
+                else :
+                    ischeck = 1
+                try:
+                    cur.execute('insert into mock_config values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',(None,lvalues[0],lvalues[4],lvalues[3],lvalues[2],lvalues[7],lvalues[5],datetime.now(),0,ischeck,lvalues[1]))
+                    conn.commit()
+                except:
+                    return jsonify({'msg': "fail", "remark": "解析失败"})
+        conn.close()
+        return jsonify({'msg': "ok", "remark": "上传成功"})
+    else:
+        return jsonify({'msg': "fail", "remark": "上传文件不符合格式要求"})
+
+@app.route('/addinfo',methods=['POST'])
+def query_user():
+    parser = reqparse.RequestParser()
+    parser.add_argument('title', type=str,required=True)
+    parser.add_argument('method', type=str,required=True)
+    parser.add_argument('reqparams', type=str, required=True)
+    parser.add_argument('resparams', type=str, required=True)
+    parser.add_argument('des', type=str)
+    parser.add_argument('domain', type=str,required=True)
+    parser.add_argument('projectName', type=str,required=True)
+    parser.add_argument('ischeck', type=int, required=True)
+    args = parser.parse_args()
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    try:
+        cur.execute('insert into mock_config (title,reqparams,methods,domain,description,resparams,status,ischeck,project_name) values (%s,%s,%s,%s,%s,%s,%s,%s,%s) ',(args.get('title'),args.get('reqparams'),args.get('method'),args.get('domain'),args.get('des'),args.get('resparams'), 0 , args.get('ischeck'),args.get('projectName')))
+        conn.commit()
+        conn.close()
+    except :
+        return jsonify({'msg': "fail", "remark": "新增数据失败"})
+    return jsonify({'msg': "ok","remark":""})
+
+@app.route('/delinfo',methods=['POST'])
+def delinfo():
+    parser = reqparse.RequestParser()
+    parser.add_argument('id[]', type=str, required=True,action='append')
+    args = parser.parse_args()
+    id=args.get('id[]')
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    try:
+        for index in range(len(id)):
+            idd=id[index]
+            cur.execute('delete from mock_config where id=%s',(idd))
+        conn.commit()
+        conn.close()
+    except :
+        return jsonify({'msg': "fail", "remark": "删除数据失败"})
+    return jsonify({'msg': "ok","remark":""})
+
+@app.route('/editinfo',methods=['POST'])
+def editinfo():
+    parser = reqparse.RequestParser()
+    parser.add_argument('title', type=str, required=True)
+    parser.add_argument('method', type=str, required=True)
+    parser.add_argument('reqparams', type=str, required=True)
+    parser.add_argument('resparams', type=str, required=True)
+    parser.add_argument('des', type=str)
+    parser.add_argument('domain', type=str, required=True)
+    parser.add_argument('id', type=int, required=True)
+    parser.add_argument('projectName', type=str, required=True)
+    parser.add_argument('ischeck', type=int, required=True)
+    args = parser.parse_args()
+    try:
+        conn = pymysql.connect(**config)
+        cur = conn.cursor()
+        cur.execute('update mock_config set title=%s,reqparams=%s,methods=%s,domain=%s,description=%s,resparams=%s,update_time=%s ,project_name=%s ,ischeck=%s'
+                    ' where id=%s',(args.get('title'),args.get('reqparams'),args.get('method'),args.get('domain'), args.get('des'), args.get('resparams'),
+                                   datetime.now().strftime('%y-%m-%d %H:%M:%S'),args.get('projectName'),args.get('ischeck'),args.get('id')))
+        conn.commit()
+        conn.close()
+    except:
+        return jsonify({'msg': "fail", "remark": "编辑数据失败"})
+    return jsonify({'msg': "ok", "remark": ""})
+
+@app.route('/selectinfo',methods=['GET'])
+def selectinfo():
+    parser = reqparse.RequestParser()
+    parser.add_argument('id', type=int, required=True)
+    args = parser.parse_args()
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    try:
+        cur.execute('select title,reqparams,methods,domain,description,resparams,project_name,ischeck from mock_config where id=%s',(args.get('id')))
+        re= cur.fetchall()
+        conn.close()
+        key = ('title', 'reqparams', 'methods', 'domain', 'description', 'resparams','project_name','ischeck')
+        d = [dict(zip(key, value)) for value in re]
+    except:
+        return jsonify({'msg': "fail", "remark": "查询信息失败"})
+    return jsonify({'msg': "ok", "remark": "", 'data':d})
+
+@app.route('/manage',methods=['POST'])
+def manage():
+    parser = reqparse.RequestParser()
+    parser.add_argument('id', type=int, required=True)
+    parser.add_argument('status', type=int, required=True)
+    args = parser.parse_args()
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    try:
+        cur.execute('update mock_config set status=%s where id=%s',(args.get('status'),args.get('id')))
+        conn.commit()
+        conn.close()
+    except:
+        return jsonify({'msg': "fail", "remark": "查询信息失败"})
+    return jsonify({'msg': "ok", "remark": ""})
+
+@app.route('/search',methods=['GET'])
+def search():
+    parser = reqparse.RequestParser()
+    parser.add_argument('title', type=str)
+    parser.add_argument('project_name', type=str, required=True)
+    args = parser.parse_args()
+    try:
+        conn = pymysql.connect(**config)
+        cur = conn.cursor()
+        if args.get('project_name') == str(0):
+            sql = "select id,status,title,reqparams,methods,domain,description,resparams,date_format(update_time,'%%Y-%%m-%%d %%H:%%i:%%s') from mock_config where title like %s"
+            value=args.get('title').strip()
+            cur.execute(sql, value+'%%')
+        else:
+            sql = "select id,status,title,reqparams,methods,domain,description,resparams,date_format(update_time,'%%Y-%%m-%%d %%H:%%i:%%s') from mock_config where title like %s and project_name=%s"
+            values = (args.get('title')+'%%' .strip(),args.get('project_name'))
+            cur.execute(sql,values)
+        re= cur.fetchall()
+        conn.close()
+        key = ('id','status','title', 'reqparams', 'methods', 'domain', 'description', 'resparams','updateTime')
+        d = [dict(zip(key, value)) for value in re]
+    except:
+        return jsonify({'msg': "fail", "remark": "select data fail"})
+    return jsonify({'msg': "ok", "remark": "", "data": d})
+
+@app.route('/searchall',methods=['GET'])
+def searchall():
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    try:
+        cur.execute("select id,title,reqparams,methods,domain,description,resparams,status,date_format(update_time,'%Y-%m-%d %H:%i:%s') from mock_config")
+        re= cur.fetchall()
+        conn.close()
+        key = ('id','title', 'reqparams', 'methods', 'domain', 'description', 'resparams','status','updateTime')
+        d = [dict(zip(key, value)) for value in re]
+    except:
+        return jsonify({'msg': "fail", "remark": "select data fail"})
+    return jsonify({'msg': "ok", "remark": "","data": d})
+
+@app.route('/searchproject',methods=['GET'])
+def searchproject():
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT DISTINCT(project_name) from mock_config where project_name <> '' or project_name is NULL")
+        re= cur.fetchall()
+        conn.close()
+    except:
+        return jsonify({'msg': "fail", "remark": "select data fail"})
+    return jsonify({'msg': "ok", "remark": "","data": re})
+
+@app.route('/copy',methods=['POST'])
+def copy():
+    parser = reqparse.RequestParser()
+    parser.add_argument('id', type=int, required=True)
+    args = parser.parse_args()
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    try:
+        cur.execute("insert into mock_config(title,reqparams,methods,domain,description,resparams,update_time,status,project_name,ischeck) "
+                    "select title,reqparams,methods,domain,description,resparams,update_time,status,project_name,ischeck from mock_config where id=%s",args.get('id'))
+        conn.commit()
+        conn.close()
+    except:
+        return jsonify({'msg': "fail", "remark": "select data fail"})
+    return jsonify({'msg': "ok", "remark": ""})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'msg':'fail','error': '404 Not found'}), 404)
+
+@app.errorhandler(500)
+def not_found(error):
+    return make_response("程序报错，可能是因为叙利亚战争导致", 500)
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',debug=True,threaded=True,port=8090)
